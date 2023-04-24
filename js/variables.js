28e0692e02714d2213e94ea6949b979193f426ce
LeonardoC1302 <lecespedes@estudiantec.cr>
2023-04-24 11:04:41 -0600

    Variables (var)

---
+++ js/variables.js
@@ -0,0 +1,18 @@
+// Variables
+var producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
+var disponible; // Iniciar variable sin valor
+
+producto = true; // Reasignando el valor de la variable
+disponible = true;
+
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+// Estilos para las variables
+var nombre_producto = 'Monitor HD'; // underscore
+var nombreProducto = 'Monitor HD'; // Camelcase
+var NombreProducto = 'Monitor HD'; // Pascal Case
+var nombreproducto = 'Monitor HD'; // Lower Case
+
+console.log(producto);
