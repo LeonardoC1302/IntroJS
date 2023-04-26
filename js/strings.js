1afd815ad49b0b2c156bb97e21d612ecf9d9bcfb
LeonardoC1302 <lecespedes@estudiantec.cr>
2023-04-26 09:54:29 -0600

    Strings

---
+++ js/strings.js
@@ -0,0 +1,14 @@
+// Strings
+const product = "Monitor 20 Pulgadas";
+const price = "30 USD";
+console.log(product)
+
+// Strings Methods
+console.log(product.length); // Length
+console.log(product.indexOf('Pulgadas')); // IndexOf
+console.log(product.includes('Tablet')); // Includes
+console.log(product.slice(0, 10)); // Slice
+console.log(product.replace('Pulgadas', '"')); // Replace
+console.log(product.split(' ')); // Split
+console.log(product.toUpperCase()); // ToUpperCase
+console.log(product.toLowerCase()); // ToLowerCase
