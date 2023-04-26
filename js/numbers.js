8f96912ae7f5be4f07e1d27fe7cc17a109d59da2
LeonardoC1302 <lecespedes@estudiantec.cr>
2023-04-26 10:03:11 -0600

    Math

---
+++ js/numbers.js
@@ -0,0 +1,12 @@
+// Numbers
+let result;
+result = Math.PI;
+result = Math.round(2.4);
+result = Math.ceil(2.4);
+result = Math.floor(2.8);
+result = Math.sqrt(144);
+result = Math.abs(-3);
+result = Math.pow(8, 2);
+result = Math.min(2, 33, 4, 1, 55, 6, 3, -2);
+result = Math.max(2, 33, 4, 1, 55, 6, 3, -2);
+result = Math.random();
