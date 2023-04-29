dc1f738f13901014f055df178fb798704cfb6ab8
LeonardoC1302 <lecespedes@estudiantec.cr>
2023-04-28 19:01:07 -0600

    If

---
+++ js/controlStructures.js
@@ -0,0 +1,25 @@
+// Control Structures
+const points = 1000;
+
+if(puntaje == 1000) {
+    console.log("Yes, it's 1000");
+} else {
+    console.log("No, it's not 1000");
+}
+
+const cash = 1000;
+const shoppingCart = 800;
+if (cash > shoppingCart) {
+    console.log("You can pay");
+} else {
+    console.log("You can't pay");
+}
+
+const role = 'ADMIN';
+if (role === 'ADMIN') {
+    console.log("You can access");
+} else if (role === 'EDITOR') {
+    console.log("You can edit");
+} else {    
+    console.log("You can't access");
+}
